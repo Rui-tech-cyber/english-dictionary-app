9710c139218251f0666e701cd59ccfdfb90d5ecc
ナカムラルイ <nakamura@nakamuraruinoMacBook-Pro.local>
2026-05-30 19:31:24 +0900

    fix diffs between md and local

--- src/app/api/words/route.ts
+++ src/app/api/words/route.ts
@@ -1,7 +1,20 @@
 import { prisma } from '@/lib/prisma'
 import { NextResponse } from 'next/server'
+import { NextRequest } from 'next/server'
 
-export async function GET() {
-  const words = await prisma.word.findMany()
+export async function GET(request: NextRequest) {
+  const query = request.nextUrl.searchParams.get('q') || ''
+
+  const words = await prisma.word.findMany({
+    where: query
+      ? {
+          OR: [
+            { word: { contains: query } },
+            { meaning: { contains: query } },
+          ],
+        }
+      : undefined,
+  })
+
   return NextResponse.json(words)
 }
